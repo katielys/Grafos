4b20c78cecbcb85646e18d122a37e8d5050ba8dd
Katiely <katiely.eu@gmail.com>
2018-08-29 21:12:07 -0400

    [INS] inserindo questoes do lab de virtu

---
+++ 1152.cpp
@@ -0,0 +1,16 @@
+//Katiely dos Santos - uri1152.cpp estradas escuras
+
+#include <iostream>
+#include <vector>
+
+struct Graph_t {
+	int edges;
+	int vertex;
+	std::vector< std::vector<int> > graph;
+};
+
+using namespace std;
+
+int main(){
+	return 0;
+}
