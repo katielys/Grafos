2ec7d08128cfc401f5c8720f1af733ee31e99bba
Katiely <katiely.eu@gmail.com>
2018-08-30 18:10:44 -0400

    [INS]modification a westeros

--- 1862.cpp
+++ 1862.cpp
@@ -1,22 +1,51 @@
-#include <iostream>
-//uri 1862 - casa de Westeros
-// MoskRocks- 30.08
-
+#include <bits/stdc++.h>
 using namespace std;
 
-char matrix[100000][100000];
 
-int main (){
-    int numberOfCases = 0;
-    //char aux;
-    
-    cin << numberOfCases; 
-    for (int i=0;i<numberOfCases;i++){
-        for (int j=0;j<numberOfCases;j++){
-            cin << matrix[i][j];
+int vertex, edges;
+
+void dfs(vector<bool> &visited, vector<vector<int> > &graph, int nowVertex) {
+    visited[nowVertex] = true;
+    vertex += 1;
+    edges += graph[nowVertex].size();
+    for(int v=0; v<graph[nowVertex].size();v++) {
+        if(!visited[v])
+            dfs(visited, graph, v);
+    }
+}
+
+int main() {
+    int v;
+    char c;
+    cin >> v;
+    vector<vector<int> > graph(v);
+    vector<bool>  visited(v);
+    for(int i=0; i<v; i++) {
+        for(int j=0; j<v; j++) {
+            cin >> c;
+            if(i != j && c == 'S')
+                graph[i].push_back(j);
         }
     }
-       
-    
+
+    vector<int> componts;
+    for(int i=0; i<v; i++) {
+        if(!visited[i]) {
+            vertex = edges = 0;
+            dfs(visited, graph, i);
+            if((vertex * (vertex - 1) != edges)) {
+                cout << "-1\n";
+                return 0;
+            }
+            componts.push_back(vertex);
+        }
+    }
+    sort(componts.rbegin(), componts.rend());
+    cout << componts.size() << '\n';
+    for(int i=0; i<componts.size(); i++) {
+        cout << componts[i];
+        if(i < componts.size() - 1) cout << ' ';
+    }
+    cout << endl;
     return 0;
 }
