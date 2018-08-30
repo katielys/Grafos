2150c3f6db62f60a730c0fc8573f7c515637a784
Katiely <katiely.eu@gmail.com>
2018-08-29 21:30:49 -0400

    [INS] Lab virtu

--- 1152.cpp
+++ 1152.cpp
@@ -9,8 +9,22 @@ struct Graph_t {
 	std::vector< std::vector<int> > graph;
 };
 
+ 
 using namespace std;
 
-int main(){
-	return 0;
+int hasCycle(Graph_t my_graph, int src, int dest){
+	return 1;
+}
+ 
+ 
+int main() {
+	int src,b,weight;
+	cin >> src;
+	cin >> b;
+	while(src!=b!=weight){
+		cin >> src;
+	}
+  
+     
+    return 0;
 }
